512b87e2b4305c27b6bde527e1977189713b2aa3
José Neto <joseneto.jn1606@gmail.com>
2024-08-19 11:52:56 -0300

    add: math controller with sum operation

---
+++ src/main/java/br/com/joseneto/springcalculator/Controllers/MathController.java
@@ -0,0 +1,31 @@
+package br.com.joseneto.springcalculator.Controllers;
+
+import br.com.joseneto.springcalculator.Exceptions.UnsupportedMathOperationException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MathController {
+
+    @RequestMapping(value ="/sum/{numberOne}/{numberTwo}", method= RequestMethod.GET)
+    public Double sum(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a numerics values");
+        }
+        return convertDouble(numberOne) + convertDouble(numberTwo);
+    }
+
+    private Double convertDouble(String number){
+        if (number == null){
+            return 0D;
+        }
+        String n = number.replace(",", ".");
+        if (isNumeric(n)) return Double.parseDouble(n);
+        return 0D;
+    }
+
+    private boolean isNumeric(String number){
+        if (number == null) return false;
+        String n = number.replaceAll(",", ".");
+        return n.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
